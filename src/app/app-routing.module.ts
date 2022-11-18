2419260eb9a0455691376b6efe8a0acbc600100a
Joe <joefuller042@gmail.com>
2022-11-18 14:12:13 +0000

    add games page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { EventPageComponent } from './components/event-page/event-page.component
 import { UserPageComponent } from './components/user-page/user-page.component';
 import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { GamesPageComponent } from './components/games-page/games-page.component';
 const routes: Routes = [
-  { path: '', redirectTo: 'sign-up', pathMatch: 'full'},
-  { path: 'sign-up', component: SignUpComponent},
-  { path: 'login', component: LoginComponent},
+  { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'events', component: EventPageComponent },
   { path: 'user', component: UserPageComponent },
   { path: 'createEvent', component: EventFormComponent },
+  { path: 'games', component: GamesPageComponent },
 ];
 
 @NgModule({
