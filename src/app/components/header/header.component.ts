f55048f239cd0373ba67e4082d568b36117a3b64
fedog-ops <felixtmcd@outlook.com>
2022-11-18 15:54:51 +0000

    add signOut(), getCurrentUser()

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/authS/auth.service';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private authService: AuthService) { }
+//authSerice in constructor has to be public, creates error when private
+username: any
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
-  }
-
+    this.getCurrentUser()
+} 
+getCurrentUser(){
+  this.authService.getCurrentUser()
+}
 }
