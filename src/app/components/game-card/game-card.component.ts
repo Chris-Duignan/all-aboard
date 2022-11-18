3645adf5114a86d8f1b181267198c6896fd753b7
Joe <joefuller042@gmail.com>
2022-11-18 14:31:25 +0000

    update game card to have a formatted title and shorter description

--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -7,8 +7,18 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GameCardComponent implements OnInit {
   @Input() game: any;
+  formattedName = '';
+  shortDescription = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formattedName = this.game.name
+      .split('-')
+      .map((word: string) => {
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(' ');
+    this.shortDescription = this.game.description.split('.')[0];
+  }
 }
