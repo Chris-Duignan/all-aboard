d7ec9ef0ed3d8922896a951c8a3d8ae2a5ef3848
Chris-Duignan <cduignan.1993@gmail.com>
2022-11-21 15:31:38 +0000

    add dotenv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  GOOGLE_MAPS_KEY : process.env["GOOGLE_MAPS_KEY"],
   firebase: {
     projectId: 'all-aboard-60792',
     appId: '1:25387963515:web:542a18bcb89cc565323ede',
     storageBucket: 'all-aboard-60792.appspot.com',
-    apiKey: 'AIzaSyB3MWL81RX61c-ITscPEq4HinhnydBIuIQ',
+    apiKey: process.env["FIREBASE_KEY"],
     authDomain: 'all-aboard-60792.firebaseapp.com',
     messagingSenderId: '25387963515',
     measurementId: 'G-7E2PK6G9PQ',
