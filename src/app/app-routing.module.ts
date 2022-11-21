862e063d20a25ca8565aea1c036def9073652a01
fedog-ops <felixtmcd@outlook.com>
2022-11-21 10:27:23 +0000

    authGuard redirects to login if not signed in. Add buttons to switch between login and sign up. Sign out redirects to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { EventPageComponent } from './components/event-page/event-page.component
 import { UserPageComponent } from './components/user-page/user-page.component';
 import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { AuthGuard, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'sign-up', pathMatch: 'full'},
   { path: 'sign-up', component: SignUpComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'events', component: EventPageComponent },
-  { path: 'user', component: UserPageComponent },
-  { path: 'createEvent', component: EventFormComponent },
+  { path: 'events', component: EventPageComponent,  canActivate: [AuthGuard], data: { authGuardPipe: () => redirectUnauthorizedTo(['login']) } },
+  { path: 'user', component: UserPageComponent, canActivate: [AuthGuard], data: { authGuardPipe: () => redirectUnauthorizedTo(['login']) } },
+  { path: 'createEvent', component: EventFormComponent, canActivate: [AuthGuard], data: { authGuardPipe: () => redirectUnauthorizedTo(['login']) } },
 ];
 
 @NgModule({
