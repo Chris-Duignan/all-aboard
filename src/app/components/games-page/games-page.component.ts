c53d876f607989ed39d0d98975b2fba2318986c0
Joe <joefuller042@gmail.com>
2022-11-22 09:16:17 +0000

    add loading to games-page

--- src/app/components/games-page/games-page.component.ts
+++ src/app/components/games-page/games-page.component.ts
@@ -9,16 +9,21 @@ import { Game } from 'src/app/classes/game';
 })
 export class GamesPageComponent implements OnInit {
   games: Game[] = [];
+  isLoading = false;
 
   constructor(private gamesService: GamesService) {}
 
   ngOnInit(): void {
-    this.getGames();
-  }
-
-  getGames(): void {
+    this.isLoading = true;
     this.gamesService.getGames().subscribe((games) => {
       this.games = games;
+      this.isLoading = false;
     });
   }
+
+  // getGames(): void {
+  //   this.gamesService.getGames().subscribe((games) => {
+  //     this.games = games;
+  //   });
+  // }
 }
