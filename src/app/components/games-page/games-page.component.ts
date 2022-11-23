a70f01ec728c6982cdc151823027cac604a6f5a1
Robyn <clockhands9@gmail.com>
2022-11-23 15:35:58 +0000

    gameslist search bar

--- src/app/components/games-page/games-page.component.ts
+++ src/app/components/games-page/games-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GamesService } from 'src/app/services/games/games.service';
 import { Game } from 'src/app/classes/game';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-games-page',
@@ -10,12 +11,32 @@ import { Game } from 'src/app/classes/game';
 export class GamesPageComponent implements OnInit {
   games: Game[] = [];
   isLoading = false;
+  search:string='';
 
-  constructor(private gamesService: GamesService) {}
+  constructor(
+    private gamesService: GamesService,
+    private _route: ActivatedRoute,
+    private _router: Router) {}
 
   ngOnInit(): void {
     this.isLoading = true;
     this.getGames();
+  }
+
+  filter(){
+    if(this.search=== ''){
+      return
+    }
+    this.games = this.games.filter((game) => {
+      return game.name.includes(this.search)
+    })
+    this._router.navigate([], {
+      relativeTo: this._route,
+      queryParams: {
+        search: this.search
+      },
+      queryParamsHandling: 'merge',
+    });
   }
 
   getGames(): void {
